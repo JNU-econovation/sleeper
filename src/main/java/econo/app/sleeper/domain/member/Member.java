4e70af2626a3a34cb72ffc752e88f1781d13a864
hwangdaesun <hds650588@gmail.com>
2023-05-07 13:48:55 +0900

    Refactor : edit Member
    I edit User class to  Member class

---
+++ src/main/java/econo/app/sleeper/domain/member/Member.java
@@ -0,0 +1,66 @@
+package econo.app.sleeper.domain.member;
+
+import econo.app.sleeper.domain.character.Character;
+import econo.app.sleeper.domain.sleep.SleepAdvisor;
+import lombok.*;
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "MEMBER")
+public class Member {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(nullable = false)
+   private String memberId;
+
+   @Column(nullable = false)
+   private String memberPassword;
+
+   @Column(length = 20, nullable = false)
+   private String memberNickName;
+
+   @Column(nullable = false)
+   private Long memberAge;
+
+   @Enumerated(EnumType.STRING)
+   @Column(name = "USER_ROLE_TYPE")
+   private RoleType roleType;
+
+   @OneToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "CHARACTER_FK")
+   private Character character;
+
+   @OneToOne(fetch =  FetchType.LAZY)
+   @JoinColumn(name = "USER_SLEEP_INFO_FK")
+   private SleepAdvisor sleepAdvisor;
+
+   @Builder
+   public Member(String memberId, String memberPassword, String memberNickName, Long memberAge, RoleType roleType){
+      this.memberId = memberId;
+      this.memberPassword = memberPassword;
+      this.memberNickName = memberNickName;
+      this.memberAge = memberAge;
+      this.roleType = roleType;
+   }
+
+   public void mappingCharacter(Character character){
+      this.character = character;
+   }
+
+   public void mappingSleepAdvisor(SleepAdvisor sleepAdvisor){
+      this.sleepAdvisor = sleepAdvisor;
+   }
+
+   public static Member createUser(String userId, String userPassword, String userNickName, Long userAge, RoleType roleType){
+      Member member = new Member(userId,userPassword,userNickName,userAge,roleType);
+      return member;
+   }
+
+
+}
